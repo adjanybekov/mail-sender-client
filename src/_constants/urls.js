2c998ffece0462d254fe24b6fdadbee934f298ea
adjanybekov <adjanybekov@gmail.com>
2020-08-15 18:16:03 +0600

    ip change

--- src/_constants/urls.js
+++ src/_constants/urls.js
@@ -1,4 +1,4 @@
-export const serverIP = "http://18.220.229.167";
+export const serverIP = "http://3.14.244.149";
 export const baseIP = serverIP + ":8080"; //'http://localhost:8090';
 
 export const urls = {
