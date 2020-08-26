97dd3f5ea900eeaf5ceeeb047146ce514e61b817
adjanybekov <adjanybekov@gmail.com>
2020-08-27 02:33:50 +0600

    removed assetss

--- src/_constants/urls.js
+++ src/_constants/urls.js
@@ -1,5 +1,5 @@
-export const serverIP = "http://3.14.244.149";
-export const baseIP = serverIP + ":8443"; //'http://localhost:8090';
+export const serverIP = "http://localhost"; //"http://3.14.244.149";
+export const baseIP = serverIP + ":8443";
 
 export const urls = {
   emailUrl: baseIP + "/api/email",
