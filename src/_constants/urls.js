6b255ce6c927c2d5ffbcb1e4bc4e922f942ea4ba
Azat Dzhanybekov <adjanybekov@gmail.com>
2021-02-23 13:06:43 +0600

    Update urls.js

--- src/_constants/urls.js
+++ src/_constants/urls.js
@@ -1,4 +1,4 @@
-export const serverIP = "http://localhost"; //"http://3.14.244.149";
+export const serverIP = "http://18.219.89.217"; //"http://3.14.244.149";
 export const baseIP = serverIP + ":8443";
 
 export const urls = {
